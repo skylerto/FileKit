ff9eb903d53fbd951f41b4f253a7ceabff15603b
skylerto <skylerclayne@gmail.com>
2015-07-07 11:38:03 -0400

    Initial Commit

---
+++ FileKit/FileKit.h
@@ -0,0 +1,19 @@
+//
+//  FileKit.h
+//  FileKit
+//
+//  Created by Skyler Layne on 2015-07-07.
+//  Copyright (c) 2015 Skyler Layne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FileKit.
+FOUNDATION_EXPORT double FileKitVersionNumber;
+
+//! Project version string for FileKit.
+FOUNDATION_EXPORT const unsigned char FileKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FileKit/PublicHeader.h>
+
+
